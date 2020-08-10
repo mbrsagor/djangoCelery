6d95811b9364ae2d9a1bff56b7b18790edf321b9
Sagor <mbrsagor@gmail.com>
2020-08-11 01:10:59 +0600

    Celery dynamic calculate models added

--- task/models.py
+++ task/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Calculator(models.Model):
+    title = models.CharField(max_length=70)
+    first_number = models.IntegerField(default=0)
+    last_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def add_number(self):
+        return self.first_number + self.last_number
+
+    @property
+    def sub_number(self):
+        return self.first_number - self.last_number
+
+    @property
+    def multiple_number(self):
+        return self.first_number * self.last_number
+
+    @property
+    def division_number(self):
+        return self.first_number / self.last_number
