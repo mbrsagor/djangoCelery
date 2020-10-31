4b7a53b25f00f9117be8a0f44e3b0c67988b8f94
Sagor <mbrsagor@gmail.com>
2020-10-31 23:10:51 +0600

    Celery timezone BD

--- CeleryTask/settings.py
+++ CeleryTask/settings.py
@@ -42,12 +42,13 @@ INSTALLED_APPS = [
     'django_celery_results'
 ]
 
+# Celery settings
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-# CELERY_TIMEZONE = TIME_ZONE
+CELERY_TIMEZONE = 'Asia/Dhaka'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
