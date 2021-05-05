3a2ce9258006b0d4555ee43a58967352f2214968
mbrsagor <mbrsagor@gmail.com>
2021-05-05 22:29:48 +0600

    Env file and db file created

---
+++ CeleryTask/db/db_config.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangocelery',
+        'USER': 'mbrsagor',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
