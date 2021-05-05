6585aa991f9754d49f5f9353d3fc755e395e39d6
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-05-05 22:04:15 +0600

    Update models.py

--- task/models.py
+++ task/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
-class Calculator(models.Model):
+class BaseEntity(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Calculator(BaseEntity):
     title = models.CharField(max_length=70)
     first_number = models.IntegerField(default=0)
     last_number = models.IntegerField(default=0)
@@ -24,3 +29,12 @@ class Calculator(models.Model):
     @property
     def division_number(self):
         return self.first_number / self.last_number
+
+    
+    class Task(BaseEntity):
+        task_name = models.CharField(max_length=100)
+        start_day = models.CharField(max_length=100)
+        
+        def __str__(self):
+            return self.task_name
+        
